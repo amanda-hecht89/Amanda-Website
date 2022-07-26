28978d7c2696639222b46a981816f4272dadbd5e
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-07-26 15:27:51 -0600

    keyframe

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import './App.css';
+import './Fancy.css';
+
 
 export default function Welcome() {
   return (
     <div>
       <div>
-        <h1>WELCOME TO MY SITE</h1>
+        <h1>WELCOME</h1>
+        <h2>AMANDA HECHT</h2>
         <img className='photo1' src='/Images/amanda1.jpeg' height='500' />
       </div>
     </div>
