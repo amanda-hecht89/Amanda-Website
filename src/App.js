fa8feb983853f532534ce55851343090e5312204
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-07-26 13:59:01 -0600

    photo

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import {
   Route,
   Link,
 } from 'react-router-dom';
+import Education from './Education';
+import ContactMe from './ContactMe';
+import Welcome from './Welcome';
 import './App.css';
 
 export default function App() {
@@ -13,22 +16,24 @@ export default function App() {
 
         <nav className="nav-header">
           <Link className="nav-items" to="/">Home</Link> 
-          <Link className='nav-items' to="/profiles">About Me</Link>
+          <Link className='nav-items' to="/education">My History</Link>
           <Link className="nav-items" to="/credits">Contact Me</Link>
         </nav>
-
-
         {/* A <Switch> looks through its children <Route>s and
             renders the first one that matches the current URL. */}
         <Switch>
           <Route exact path="/">
+            <Welcome />
           </Route>
           <Route exact path="/credits">
+            <ContactMe />
           </Route>
-          <Route exact path="/profiles">
+          <Route exact path="/education">
+            <Education/>
           </Route>
         </Switch>
       </div>
     </Router>
+
   );
 }
--- src/Welcome.js
+++ src/Welcome.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 export default function Welcome() {
   return (
-    <div>Welcome</div>
+    <div>
+      <div>
+        <h1>WELCOME TO MY SITE</h1>
+        <img className='photo1' src='/Images/amanda1.jpeg' height='500' />
+      </div>
+    </div>
   );
 }
