25ce13cf7af4884235c5421e07c006af60231698
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-07-27 09:15:58 -0600

    css edit

--- src/ContactMe.js
+++ src/ContactMe.js
@@ -6,23 +6,26 @@ export default function ContactMe() {
       <div>
         <h1>contact me</h1>
         <div className='links'>
-          <div>
+          <div className='alsolinks'>
             <h2>LinkedIn</h2>
             <a href="https://www.linkedin.com/in/amanda-hecht/" target="/blank"><img src="/images/LinkedIn-icon.png" className="icon" /></a>
+            <p>Click here for my LinkedIn Profile</p>
           </div>
-          <div>
+          <div className='alsolinks'>
             <h2>GitHub</h2>
             <a href="https://github.com/amanda-hecht89" target="/blank"><img src="/images/GitHub-icon.png" className="icon" /></a>
+            <p>Click here to go to my Github Repo</p>
           </div>
-          <div>
+          <div className='alsolinks'>
             <h2>HYNT</h2>
             <a href="https://benevolent-jalebi-781aea.netlify.app/" target="/blank"><img src="/images/hynt.png" className="icon" /></a>
-            <p>Here is a link to HYNT, a text adventure game. The first group project I ever did. Made on Vanilla JavaScript. </p>
+            <p>Here is a link to HYNT, <br /> a text adventure game.<br /> The first group project I ever did. <br /> Made on Vanilla JavaScript. </p>
           </div>
-          <div>
+          <div className='alsolinks'>
             <h2>PAAC</h2>
             <a href="https://benevolent-jalebi-781aea.netlify.app/" target="/blank"><img src="/images/paac.png" className="icon" /></a>
-            <p>Here is a link to PAAC, a saltwater outsourced database. The second group project I did. Made on React </p>
+            <p>Here is a link to PAAC, <br /> a saltwater outsourced database. <br /> The second group project I did. <br /> Made on React </p>
+
           </div>
         </div>
       </div>
