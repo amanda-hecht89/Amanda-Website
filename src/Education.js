55a53366088aaf59a72d65823dea49cae5276845
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-07-27 09:46:03 -0600

    added life

--- src/Education.js
+++ src/Education.js
@@ -8,6 +8,13 @@ export default function Education() {
         <Accordion/>
       </div>
       <h1>TRAVEL</h1>
+      <div>
+        <img src='/Images/georigatext.png' height='300' className='life'/>
+        <img src='/Images/colorado1text.png' height='300' className='life'/>
+        <img src='/Images/washtext.png' height='300' className='life'/>
+        <img src='/Images/calitext.png' height='300' className='life'/>
+        <img src='/Images/colorado2text.png' height='300' className='life'/>
+      </div>
       <img src='/Images/travel.jpeg' height='350' className='travel'/>
 
       <footer></footer>
