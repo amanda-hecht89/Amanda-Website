527017c3f60d695184f81e0a8a5673ed65d8e5f0
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-07-29 14:07:14 -0600

    added shell stoty

--- src/ShellGame.js
+++ src/ShellGame.js
@@ -21,9 +21,28 @@ export default function ShellGame() {
 
 
   return (
-    <div>SHELLGAME
-
+    <div><h1>Shell Game</h1>
+      <div className='explain'>
+        <p>
+            This game was something I learned how to do like the second week of school<br/>
+            on Vanilla JavaScript. Once I learned ReAct, I decided this would be a fun thing <br/>
+            to learn to rebuid in ReAct for use in my portfolio. ENJOY!
+        </p>
+        <p>
+            Welcome to Colorado! I love living here and doing all the wonderful things there <br/>
+            there is to do here, include hiking. But be careful, you might spot a YETI!!!!!
+        </p>
+      </div>
       <div className={ reveal ? 'reveal' : 'hide' }>
+        <div className='shellOne'>
+            SHELL 1
+        </div>
+        <div className='shellTwo'>
+            SHELL 2
+        </div>
+        <div className='shellThree'>
+            SHELL 3
+        </div>
 
       </div>
     </div>
