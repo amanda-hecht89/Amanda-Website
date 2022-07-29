15976205fca94d492e46bbf499b7d991ad13f004
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-07-29 15:18:38 -0600

    more shell game

--- src/ShellGame.js
+++ src/ShellGame.js
@@ -21,29 +21,44 @@ export default function ShellGame() {
 
 
   return (
-    <div><h1>Shell Game</h1>
+    <div>
+      <h1>Shell Game</h1>
       <div className='explain'>
-        <p>
-            This game was something I learned how to do like the second week of school<br/>
-            on Vanilla JavaScript. Once I learned ReAct, I decided this would be a fun thing <br/>
+        <p className='para3'>
+            This game was something I learned how to do the second week of school
+            on Vanilla JavaScript.<br/> Once I learned ReAct, I decided this would be a fun thing
             to learn to rebuid in ReAct for use in my portfolio. ENJOY!
         </p>
-        <p>
-            Welcome to Colorado! I love living here and doing all the wonderful things there <br/>
+        <p className='para3'>
+            Welcome to Colorado! I love living here and doing all the wonderful things there
             there is to do here, include hiking. But be careful, you might spot a YETI!!!!!
         </p>
       </div>
       <div className={ reveal ? 'reveal' : 'hide' }>
         <div className='shellOne'>
-            SHELL 1
+            ASPEN
+          <img className='shell' src='./Images/colorado2.png' height='200'/>
+          <img className='ball' src='./Images/yeti.png' height='150'/>
+          <div className='shellButton'>
+            <button className='button1'>PICK HERE</button>
+          </div>
         </div>
         <div className='shellTwo'>
-            SHELL 2
+            ESTES PARK
+          <img className='shell' src='./Images/colorado2.png' height='200'/>
+          <img className='ball' src='./Images/yeti.png' height='150'/>
+          <div className='shellButton'>
+            <button className='button1'>PICK HERE</button>
+          </div>
         </div>
         <div className='shellThree'>
-            SHELL 3
+            BRECKENRIDGE
+          <img className='shell' src='./Images/colorado2.png' height='200'/>
+          <img className='ball' src='./Images/yeti.png' height='150'/>
+          <div className='shellButton'>
+            <button className='button1'>PICK HERE</button>
+          </div>
         </div>
-
       </div>
     </div>
   );
