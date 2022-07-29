8e11fbc9cd659bb310a7a53ee116a8397e4a0d5a
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-07-29 11:48:34 -0600

    another paragraph

--- src/Welcome.js
+++ src/Welcome.js
@@ -8,19 +8,28 @@ export default function Welcome() {
     <div>
 
 
-      <div className='WelHead'>
+      <div className='welcome'>
         <h1>WELCOME</h1>
         <h2>AMANDA HECHT</h2>
-        <img className='photo1' src='/Images/amanda1.jpeg' height='500' />
+        <div className='about'>
+          <img className='photo1' src='/Images/amanda1.jpeg 'height='475' width='400'/>
+          <div className='paragraph'>
+            <p className='para1'>
+            Im a Software Engineer with a background in Barbering and Beauty.<br /> 
+            Im a creative person and a huge animal lover.<br/> 
+            Making lots of awesome changes in my life and ready to start a new chapter in my life.
+            </p>
+            <p className='para2'>
+            This website is a working portfolio of everything that I can do. <br/>
+            React, CSS, HTML, Material UI, Keyframes, etc! <br/>
+            I hope you enjoy looking at it as much as I have making it! <br/>
+            If you have any questions please reach out! <br/>
+            Id love to hear from you.
+            </p>
+          </div>
+        </div>
       </div>
-
-      <div className='WelBody'>
-        <p className='para1'>
-        Im a Software Engineer with a background in Barbering and Beauty. Im a creative person and a huge animal lover. Making lots of awesome changes in my life and ready to start a new chapter in my life.
-        </p>
-      </div>
-
-
+      <footer></footer>
     </div>
   );
 }
